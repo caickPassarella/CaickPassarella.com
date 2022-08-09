fce6d346f2fb4ed91adcbcb1ae81de2c5912efbf
Caick <caickpassarella@hotmail.com>
2022-08-09 18:42:14 -0300

    Fixed image file name

--- src/images/index.js
+++ src/images/index.js
@@ -3,7 +3,7 @@ import CoffeeHand from './coffeeHand.png';
 import Drawing from './drawing.png';
 import Latte from './latte.png';
 import Laptop from './laptop.png';
-import Lightbulb from './lightbulb2.png';
+import Lightbulb from './lightbulb.png';
 import Notes from './notes.png';
 import Men from './men.jpg';
 
